b453b66b9c4f47a98bc4029966ea5fc891a15bb0
Jimmy Hansard <jimmy.hansard@hotmail.com>
2022-12-16 20:36:39 +0800

    Hello

--- deploy.js
+++ deploy.js
@@ -1,8 +1,14 @@
 const ethers = require('ethers');
 
+const RPC_SERVER='HTTP://192.168.8.88:8545';
+const PRIVATE_KEY='02068f420a2391dabeb91fa879d5dcb4212719dace9d60412ecd3810a39cb8fa';
+
+
 async function main() {
     console.log("Hello from main()!");
-    const provider = ethers.providers.JsonRpcProvider();
+    const provider = ethers.providers.JsonRpcProvider(RPC_SERVER);
+    let block_number = await provider.getBlockNumber();
+    console.log('Block number:${block_number}');
     
 }
 
